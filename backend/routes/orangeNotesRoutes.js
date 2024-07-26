eb06a13fa18ae05400737b021c6b1ddd0dcc4efb
Amit Ranjan <122173165+iamakr97@users.noreply.github.com>
2024-07-26 11:15:15 +0530

    Update orangeNotesRoutes.js

--- backend/routes/orangeNotesRoutes.js
+++ backend/routes/orangeNotesRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+   
 const { signupUser } = require('../controllers/signupUser');
 const { loginUser, logoutUser } = require('../controllers/loginUser');
 const { auth, isUser } = require('../middlewares/auth');
